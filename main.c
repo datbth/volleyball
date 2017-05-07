c1f4ec0c4006a6063d37d6a8fd5f72c84ac7bee9
Dzũng Lê <morepositive@outlook.com>
2017-05-07 15:19:15 +0700

    commited mac changes

--- main.c
+++ main.c
@@ -9,7 +9,8 @@ and may not be redistributed without written permission.*/
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_mixer.h>
-#include <SDL2/SDL_ttf.h>
+//#include <SDL2/SDL_ttf.h>
+#include <SDL_ttf.h>
 #include "object.h"
 #include "main.h"
 
