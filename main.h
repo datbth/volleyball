f1528da0a1396af66e2da30571381c803899c7b3
easd <scott.bui97@gmail.com>
2017-05-14 23:57:54 +0700

    improve doc on main.h

--- main.h
+++ main.h
@@ -19,69 +19,75 @@ SDL_Texture *loadTexture(char *path);
 SDL_Window *init();
 
 /**
- * handle keys when they are pressed
- * move players
- * quit when Ctrl + Q are pressed
- * @return 0
+ * check which key combination is pressed and pass the key state to every Player's move() function
+ * @return 0 - none
+ *          1 - Ctrl Q
+ *          2 - Ctrl R
+ *          3 - Ctrl P
+ *          4 - Ctrl U
+ *          5 - Return
  */
 int handleKeys();
 
 /**
- * find if an element in an array of integer
+ * check if an integer exists in an array
  * @param arr : pointer to integer array
  * @param elem : integer element
- * @param length : length of array in integer
+ * @param length : length of array
  * @return true if element is in array/ false if not
  */
 bool isElemInArray(int* arr, int elem, int length);
 
 /**
- *
+ * Find the index of the first integer in a char array
  * @param array pointer to array of character
- * @return -1 if there is integer in array / 0 if nothing in array
+ * @return -1 if no integer found / otherwise return the index of the first integer
  */
 int getIntFromArray(char * array);
 
 /**
- * reset positions
+ * reset all players and the wall to the initial positions and remove velocity. reset the ball to top of the screen
+ * (the wall is also resetted because the team has planned to allow users to move the wall, as a hidden feature, in the future)
+ * if teamHasBall == 0, ball will be dropped on a random side
  * @param teamHasBall integer to determine which team has ball
  */
 void resetPositions(int teamHasBall);
 
 /**
- *
+ * generate a char array that displays the score of both teams
  * @param string pointer to string
- * @param teamPoint1st integer of point team 1
- * @param teamPoint2nd integer of point team 2
+ * @param teamPoint1st team 1's point
+ * @param teamPoint2nd team 2's point
  */
 void getScoreString(char *string, int teamPoint1st, int teamPoint2nd);
 
 /**
- *
- * @param ballId integer, id of a ball object
- * @param teamPoint1st pointer to integer point team 1
- * @param teamPoint2nd pointer to integer point team 2
- * @param winningTeam pointer to integer win 
+ * check the position of the ball to determine whether a team has scored a point
+ * @param ballId id of the ball object
+ * @param teamPoint1st team 1's point
+ * @param teamPoint2nd team 2's point
+ * @param winningTeam the number of the team who has won the game
+ * @return 0 if neither team has scored, otherwise return the number of the team that loses the point and is going to receive the ball in the next round
  */
 int updateScore(int ballId, int *teamPoint1st, int *teamPoint2nd, int *winningTeam);
 
 /**
- *
- * @param color Color of SDL
- * @param fontX integer of font X
- * @param fontY interger of font Y
+ * show the game instructions
+ * @param color color of the instruction text
+ * @param fontX X cordinate of the instruction
+ * @param fontY Y cordinate of the instruction
  * @param background pointer to background
  */
 void showInstruction(SDL_Color color, int fontX, int fontY, SDL_Rect *background);
 
 /**
- * Pause the game
+ * Pause/unpause the game
  */
 void togglePause();
 
 /**
  * remove items
- * @param checkNeedRemove boolean
+ * @param checkNeedRemove if true, the function will check and only remove the collided items; otherwise remove all items
  */
 void cleanItems(bool checkNeedRemove);
 
