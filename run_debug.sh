bdee12bce3f0f9483d1bf2cfddd8ba32e80f590f
Hai Nguyen <hainguyen.hainguyen1210@gmail.com>
2017-12-19 05:24:48 +0700

    update install script

--- run_debug.sh
+++ run_debug.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get -y install cmake libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev
+sudo apt-get -y install cmake gcc g++ libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev
 mkdir cmake-build-debug
 cd cmake-build-debug
 cmake ..
